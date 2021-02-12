7da36e126af969bf193ddc88da1c71c189e5cf0f
Pito Salas <pitosalas@gmail.com>
2021-02-12 16:27:45 +0000

    more

--- src/pa_starters/roomba_starter.py
+++ src/pa_starters/roomba_starter.py
@@ -7,6 +7,7 @@ import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
+import numpy as np
 
 def scan_cb(msg):
     global ranges
@@ -24,17 +25,19 @@ odom_sub = rospy.Subscriber('/odom', Odometry, odom_cb)
 pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
 
 pose = None
-start_time = rospy.Time.now()
 rate = rospy.Rate(2)
 
-# while pose == None:
-#     print("Waiting for simulated robot")
+start_time = rospy.Time.now()
+while pose == None:
+    print("Waiting for simulated robot")
 
 start_pose = pose
 
 while not rospy.is_shutdown():
-    if rospy.Time.now() - start_time < rospy.Duration(secs=5):
+    if rospy.Time.now() - start_time < rospy.Duration(secs=30):
+        print(pose.pose.position.x)
         print(">>>>>>>>>>> roomba is vaccuming")
     else:
         print("<<<<<<<<<< roomba returns to start")
         break
+    rate.sleep()
