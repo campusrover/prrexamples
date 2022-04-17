2fd316f5a62425887ecfa43d91929ae074366547
Pito Salas <pitosalas@gmail.com>
2022-04-17 20:48:58 +0000

    before bug change

--- src/robogym.py
+++ src/robogym.py
@@ -9,26 +9,88 @@ from prrexamples.msg import Robogym
 
 class RoboGym:
     def __init__(self):
-        self.speed_trans = 0
-        self.speed_ang = 0
-        self.pub_rate = 0          # How many pubs happen per second
         self.pub_cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
         self.sub_cli = rospy.Subscriber('cli', Robogym, self.cli_callback)
+        self.reset()
+
+    def reset(self):
+        self.speed_trans = 0
+        self.mode = "standard"
+        self.countdown = 0
+        self.speed_ang = 0
+        self.speed_linear = 0
+        self.pub_rate = 0.5
+        self.pub_cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+        self.sub_cli = rospy.Subscriber('cli', Robogym, self.cli_callback)
+        self.last_twist = Twist()
+
+
+# Modes are:
+# standard - publishes cmd_vel 'rate' times per second
+# idle - doesn't publish cmd_vel
+# limited - goes for a certain count, time or distance
+#
+# params are:
+# rate = hz for cmd_vel pubs
+# lin = linear speed
+# ang = angular speed
+# couunt = counter 
 
     def cli_callback(self, msg):
-        pass
+        if (msg.command == "rate"):
+            self.pub_rate = msg.arg1
+            if self.pub_rate == 0:
+                self.mode = "idle"
+        elif (msg.command == "lin"):
+            self.speed_linear = msg.arg1
+        elif msg.command == "rot":
+            self.speed_ang = msg.arg1
+        elif msg.command == "count":
+            self.countdown = msg.arg1
+        elif msg.command == "reset":
+            self.reset()
+        elif msg.command == "standard":
+            self.mode = "standard"
+            self.pub_rate = 0.5
+        elif msg.command == "idle":
+            self.mode = "idle"
+        elif msg.command == "limit":
+            self.mode = "limit"
+
+
+    def step(self):
+        print("publish cmd_vel")
+        self.last_twist = Twist()
+        self.last_twist.linear.x = self.speed_linear
+        self.last_twist.angular.z = self.speed_ang
+        self.pub_cmd_vel.publish(self.last_twist)
+        if self.pub_rate == 0:
+            print("step with pub rate 0 is a bug")
+        else:
+            rospy.sleep(1.0/self.pub_rate)
 
     def run(self):
-        pass
+        while not rospy.is_shutdown():
+            if self.mode == "limit":
+                print("counting down")
+                self.countdown -= 1
+                if self.countdown == 0:
+                    self.mode = "idle"
+                    self.pub_rate = 0.0
+                else:
+                    self.step()
+            elif self.mode == "idle":
+                print("idle")
+                rospy.sleep(1)
+            elif self.mode == "standard":
+                self.step()
 
 # Main function.
 if __name__ == "__main__":
     # Initialize the node and name it.
     rospy.init_node("robogym")
     try:
-        RoboGym()
+        RoboGym().run()
     except rospy.ROSInterruptException:
         pass
-    rospy.spin()
-
-
+    
--- src/robogym_cmds.py
+++ src/robogym_cmds.py
@@ -14,11 +14,11 @@ class RobogymCmds:
     def process_command(self, text_line):
         command = text_line.split()
         message = Robogym()
-        if command[0] == "help":
-            print("Commands are: reset, stop, exit, once, rate, trans, rot, status")
-        elif command[0] not in {'reset', 'once', 'status', 'rate', 'trans', 'rot'}:
+        if text_line == "" or command[0] == "help":
+            print("Commands are: reset, stop, exit, once, rate, lin, ang, status, pub, count, time, standard, idle, limit")
+        elif command[0] not in {'reset', 'once', 'status', 'rate', 'lin', 'ang', 'pub', 'count', 'time','idle', 'limit', 'standard'}:
             print("I don't know that command")
-            print("Commands are: reset, exit, once, rate, trans, rot, status")
+            print("Commands are: reset, exit, once, rate, lin, ang, status, pub, count, time, idle, standard, limit")
         else:
             message.command = command[0]
             if (len(command) >= 2):
@@ -28,28 +28,32 @@ class RobogymCmds:
             self.cli_pub.publish(message)
 
     def run(self):
-        rate = rospy.Rate(100)
-        old_attr = termios.tcgetattr(sys.stdin)
+        rate = rospy.Rate(5)
+        #old_attr = termios.tcgetattr(sys.stdin)
         try:
-            tty.setcbreak(sys.stdin.fileno())
+            #tty.setcbreak(sys.stdin.fileno())
             command = ""
             print("\n> ", end="", flush=True)
             while not rospy.is_shutdown():
                 if select.select([sys.stdin], [], [], 0)[0] == [sys.stdin]:
-                    char = sys.stdin.read(1)
-                    if char == "\n":
-                        self.process_command(command)
-                        print("\n> ", end="", flush=True)
-                        command = ""
-                    elif ord(char) == 127:
-                        command = command[0:-1]
-                        print("\r" + "> " + command, end="")
-                    else:
-                        print(char, end="", flush=True)
-                        command = command + char
+                    command = input("> ")
+                    self.process_command(command)
+
+                    # char = sys.stdin.read(1)
+                    # if char == "\n":
+                    #     self.process_command(command)
+                    #     print("\n> ", end="", flush=True)
+                    #     command = ""
+                    # elif ord(char) == 127:
+                    #     command = command[0:-1]
+                    #     print("\r" + "> " + command, end="")
+                    # else:
+                    #     print(char, end="", flush=True)
+                    #     command = command + char
                 rate.sleep()
         finally:
-            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_attr)
+            pass
+        #     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_attr)
 
 
 if __name__ == "__main__":
