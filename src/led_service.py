258958dec9a986982a46c92187b316a02c5420c3
Pito Salas <pitosalas@gmail.com>
2022-05-10 12:47:46 -0400

    fixed commenting

--- src/led_service.py
+++ src/led_service.py
@@ -4,8 +4,8 @@ import rospy
 from std_srvs.srv import SetBool
 import RPi.GPIO as GPIO
 
-LED_GPIO = 4 // Yellow
-# LED_GPIO = 3 // Green
+LED_GPIO = 4 #  Yellow
+# LED_GPIO = 3   Green
 # LED_GPIO = 2 // Blue
 # LED_GPIO = 17 // White
 # LED_GPIO = 27 // Red
